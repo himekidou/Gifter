e13ff4a433bbd249b666601b7c85194c5466e17e
user <user@DESKTOP-01ND8VV>
2020-03-19 09:42:00 +0900

    Test Page

--- Gifter/src/main/java/com/swdo/gift/controller/MemberController.java
+++ Gifter/src/main/java/com/swdo/gift/controller/MemberController.java
@@ -33,6 +33,7 @@ public class MemberController {
 	@Autowired
 	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
 		this.naverLoginBO = naverLoginBO;
+		
 	}
 
 		
@@ -42,19 +43,19 @@ public class MemberController {
 	@Autowired
 	private UserMailSendService mailsender;
 
-	//회원가입 폼 이동
+	//�쉶�썝媛��엯 �뤌 �씠�룞
 	@RequestMapping(value="signupForm", method=RequestMethod.GET)
 	public String signupForm() {
-		logger.info("회원가입 페이지 이동");
+		logger.info("�쉶�썝媛��엯 �럹�씠吏� �씠�룞");
 		return "member/signupForm";
 	}
 		
-	//회원가입
+	//�쉶�썝媛��엯
 	@RequestMapping(value="signup", method=RequestMethod.POST)
 	public String signup(Member member, Model model, HttpServletRequest request) {
-		logger.info("회원가입 ");
+		logger.info("�쉶�썝媛��엯 ");
 	
-		//회원가입 메서드
+		//�쉶�썝媛��엯 硫붿꽌�뱶
 		int cnt = dao.memberInsert(member);
 		
 		logger.info(Integer.toString(cnt));
@@ -67,25 +68,25 @@ public class MemberController {
 	//callback
 	@RequestMapping(value="callback", method=RequestMethod.GET)
 	public String callback(HttpSession session) {
-		logger.info("콜백");	
+		logger.info("肄쒕갚");	
 			
-		//네이버로 로그인한 사람의 닉네임(member_id)과 이메일(member_email)을 가져온다
-		//닉네임(member_id)을 session에 넣는다
-		//메인으로 돌아간다.	
+		//�꽕�씠踰꾨줈 濡쒓렇�씤�븳 �궗�엺�쓽 �땳�꽕�엫(member_id)怨� �씠硫붿씪(member_email)�쓣 媛��졇�삩�떎
+		//�땳�꽕�엫(member_id)�쓣 session�뿉 �꽔�뒗�떎
+		//硫붿씤�쑝濡� �룎�븘媛꾨떎.	
 		
 		return "redirect:/";
 	}
 	
 	
 	
-	//이메일 인증 시키기
+	//�씠硫붿씪 �씤利� �떆�궎湲�
 	@RequestMapping(value="beforeReg", method=RequestMethod.GET)
 	public String beforeReg() {	
-		logger.info("이메일 인증 요구");
+		logger.info("�씠硫붿씪 �씤利� �슂援�");
 		return "member/beforeReg";
 	}
 	
-	//e-mail 인증 컨트롤러
+	//e-mail �씤利� 而⑦듃濡ㅻ윭
 	@RequestMapping(value="key_alter", method=RequestMethod.GET)
 	public String key_alterConfirm(@RequestParam("member_id") String member_id
 			,@RequestParam("member_key") String member_key ) {
@@ -95,37 +96,37 @@ public class MemberController {
 		return "member/regSuccess";
 	}
 	
-	//로그인 폼 이동
+	//濡쒓렇�씤 �뤌 �씠�룞
 	@RequestMapping(value="loginForm", method=RequestMethod.GET)
 	public String loginForm() {
-		logger.info("로그인 폼 이동");
+		logger.info("濡쒓렇�씤 �뤌 �씠�룞");
 		return "member/loginForm";		
 	}
 		
-	//로그인 하기
+	//濡쒓렇�씤 �븯湲�
 	@RequestMapping(value="login", method= RequestMethod.GET)
 	public String login(Member member, HttpSession session, Model model) {
-		logger.info("로그인 하기");
+		logger.info("濡쒓렇�씤 �븯湲�");
 		int result = dao.memberLogin(member);
 		if(result != 1) {
-			logger.info("로그인 실패");
+			logger.info("濡쒓렇�씤 �떎�뙣");
 			return "redirect:/member/loginForm";
 		}
-		logger.info("로그인 성공");
+		logger.info("濡쒓렇�씤 �꽦怨�");
 		session.setAttribute("member_id", member.getMember_id());
 		return "redirect:/";
 	}
 	
-	//네이버 로그인
+	//�꽕�씠踰� 濡쒓렇�씤
 	@RequestMapping(value="naverLogin", method = RequestMethod.GET)
 	public String naverLogin(Model model, HttpSession session) {
 		
-		/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
+		/* �꽕�씠踰꾩븘�씠�뵒濡� �씤利� URL�쓣 �깮�꽦�븯湲� �쐞�븯�뿬 naverLoginBO�겢�옒�뒪�쓽 getAuthorizationUrl硫붿냼�뱶 �샇異� */
 		String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
 		//https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=sE***************&
 		//redirect_uri=http%3A%2F%2F211.63.89.90%3A8090%2Flogin_project%2Fcallback&state=e68c269c-5ba9-4c31-85da-54c16c658125
-		logger.info("네이버: " + naverAuthUrl);
-		//네이버
+		logger.info("�꽕�씠踰�: " + naverAuthUrl);
+		//�꽕�씠踰�
 		model.addAttribute("url", naverAuthUrl);
 
 
@@ -133,10 +134,10 @@ public class MemberController {
 	}
 	
 		
-	//로그아웃
+	//濡쒓렇�븘�썐
 	@RequestMapping(value="logout", method=RequestMethod.GET)
 	public String logout(HttpSession session) {
-		logger.info("로그아웃 하기");
+		logger.info("濡쒓렇�븘�썐 �븯湲�");
 		session.removeAttribute("member_id");
 		return "redirect:/";		
 	}	
